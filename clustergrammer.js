f1052a735297f40b44dfb2d0d6cf2e483ae1475f
cornhundred <nickfloresfernandez@gmail.com>
2017-01-07 12:39:41 -0500

    using latest clustergrammer.js with lower border tolerances and partly working category breakdown

--- clustergrammer.js
+++ clustergrammer.js
@@ -1077,7 +1077,7 @@ var Clustergrammer =
 	  }
 
 	  // width is 1 over this value
-	  viz.border_fraction = 55;
+	  viz.border_fraction = 65;
 	  viz.uni_margin = 5;
 
 	  viz.super_labels = {};
@@ -1223,9 +1223,6 @@ var Clustergrammer =
 	            } else {
 	              viz.cat_names[inst_rc][inst_cat] = inst_cat;
 	            }
-
-	            // ////////////////////////////
-	            // viz.cat_names[inst_rc][inst_cat] = inst_cat;
 	          } else {
 	            viz.cat_names[inst_rc][inst_cat] = inst_cat;
 	          }
@@ -1235,11 +1232,6 @@ var Clustergrammer =
 
 	        // check whether all the categories are of value type
 	        inst_info = check_if_value_cats(cat_states);
-
-	        // // !!! tmp disable value categories
-	        // ///////////////////////////////////
-	        // ///////////////////////////////////
-	        // inst_info.type = 'cat_strings';
 
 	        // pass info_info object
 	        viz.cat_info[inst_rc][inst_cat] = inst_info;
@@ -3554,7 +3546,7 @@ var Clustergrammer =
 	      inst_rc = 'both';
 	    }
 
-	    dendro_mouseover(cgm, this);
+	    dendro_mouseover(cgm, this, d, inst_rc);
 	    dendro_group_highlight(params, this, d, inst_rc);
 	    dendro_tip.show(d);
 
@@ -3810,17 +3802,47 @@ var Clustergrammer =
 
 /***/ },
 /* 60 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	module.exports = function dendro_mouseover(cgm, inst_selection) {
+	var calc_cluster_cat_breakdown = __webpack_require__(200);
+
+	module.exports = function dendro_mouseover(cgm, inst_selection, inst_data, inst_rc) {
+
+	  var params = cgm.params;
 
 	  // run instantly on mouseover
 	  d3.select(inst_selection).classed('hovering', true);
 
 	  if (cgm.params.dendro_callback != null) {
 	    cgm.params.dendro_callback(inst_selection);
+	  }
+
+	  var cat_breakdown = calc_cluster_cat_breakdown(cgm.params, inst_data, inst_rc);
+
+	  // loop through cat_breakdown data
+	  var inst_breakdown;
+	  var bar_data;
+	  var tmp_fraction;
+	  var tmp_name;
+	  for (var i = 0; i < cat_breakdown.length; i++) {
+
+	    inst_breakdown = cat_breakdown[i];
+
+	    bar_data = inst_breakdown.bar_data;
+
+	    for (var x = 0; x < bar_data.length; x++) {
+	      // data for individual bar
+	      var tmp_data = bar_data[x];
+
+	      tmp_name = bar_data[x][0];
+	      tmp_fraction = bar_data[x][1];
+
+	      console.log(tmp_name + ' ' + String(tmp_fraction));
+	    }
+
+	    console.log('----------------\n');
 	  }
 		};
 
@@ -10391,13 +10413,6 @@ var Clustergrammer =
 	  // recalculate the visualization parameters using the updated network_data
 	  tmp_cgm.params = calc_viz_params(tmp_cgm.params, false);
 
-	  // // set up zoom (zoom is modified by room for categories)
-	  // tmp_cgm.params.zoom_behavior = d3.behavior.zoom()
-	  //   .scaleExtent([1, tmp_cgm.params.viz.real_zoom * tmp_cgm.params.viz.zoom_switch])
-	  //   .on('zoom', function(){
-	  //     zoomed(tmp_cgm);
-	  //   });
-
 	  make_row_cat(tmp_cgm, true);
 	  resize_viz(tmp_cgm);
 
@@ -13246,6 +13261,123 @@ var Clustergrammer =
 	  //   value:1.0
 	  // });
 		};
+
+/***/ },
+/* 200 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function calc_cluster_cat_breakdonw(params, inst_data, inst_rc) {
+	  // Category-breakdown of dendrogram-clusters
+	  /////////////////////////////////////////////
+	  /*
+	   1. get information for nodes in cluster
+	  2. find category-types that are string-type
+	  3. count instances of each category name for each category-type
+	   */
+
+	  // 1: get information for nodes in cluster
+	  ///////////////////////////////////////////
+
+	  // names of nodes in cluster
+	  var clust_names = inst_data.all_names;
+	  // array of nodes in the cluster
+	  var clust_nodes = [];
+
+	  var all_nodes = params.network_data[inst_rc + '_nodes'];
+
+	  var inst_name;
+	  _.each(all_nodes, function (inst_node) {
+
+	    inst_name = inst_node.name;
+
+	    if (clust_names.indexOf(inst_name) >= 0) {
+	      clust_nodes.push(inst_node);
+	    }
+	  });
+
+	  // 2: find category-types that are string-type
+	  ///////////////////////////////////////////////
+
+	  var inst_cat_info = params.viz.cat_info[inst_rc];
+
+	  // tmp list of all categories
+	  var tmp_types_index = _.keys(inst_cat_info);
+	  // this will hold the indexes of string-type categories
+	  var cat_types_index = [];
+
+	  // var inst_node = params.network_data[inst_rc+'_nodes'][0];
+
+	  // get category names (only include string-type categories)
+	  var cat_types_names = [];
+	  var type_name;
+	  var inst_index;
+	  var cat_index;
+	  for (var i = 0; i < tmp_types_index.length; i++) {
+
+	    cat_index = 'cat-' + String(i);
+
+	    if (params.viz.cat_info[inst_rc][cat_index].type === 'cat_strings') {
+	      type_name = params.viz.cat_names[inst_rc][cat_index];
+	      cat_types_names.push(type_name);
+	      cat_types_index.push(cat_index);
+	    }
+	  }
+
+	  var tmp_run_count = {};
+	  var cat_breakdown = [];
+	  var inst_breakdown = {};
+	  var bar_data;
+
+	  if (cat_types_names.length > 0) {
+
+	    // 3: count instances of each category name for each category-type
+	    var cat_name;
+	    var num_in_clust = clust_names.length;
+	    _.each(cat_types_index, function (cat_index) {
+
+	      inst_index = cat_index.split('-')[1];
+	      type_name = cat_types_names[inst_index];
+
+	      tmp_run_count[type_name] = {};
+
+	      // loop throught nodes and keep running count of categories
+	      _.each(clust_nodes, function (tmp_node) {
+
+	        cat_name = tmp_node[cat_index];
+
+	        if (cat_name.indexOf(': ') >= 0) {
+	          cat_name = cat_name.split(': ')[1];
+	        }
+
+	        if (cat_name in tmp_run_count[type_name]) {
+	          tmp_run_count[type_name][cat_name] = tmp_run_count[type_name][cat_name] + 1 / num_in_clust;
+	        } else {
+	          tmp_run_count[type_name][cat_name] = 1 / num_in_clust;
+	        }
+	      });
+
+	      inst_breakdown = {};
+	      inst_breakdown['type_name'] = type_name;
+
+	      // sort cat info in cat_breakdown
+	      bar_data = [];
+	      var maxSpeed = tmp_run_count[type_name];
+	      for (var vehicle in maxSpeed) {
+	        bar_data.push([vehicle, maxSpeed[vehicle]]);
+	      }bar_data.sort(function (a, b) {
+	        return b[1] - a[1];
+	      });
+
+	      inst_breakdown['bar_data'] = bar_data;
+
+	      cat_breakdown.push(inst_breakdown);
+	    });
+	  }
+
+	  return cat_breakdown;
+	};
 
 /***/ }
 /******/ ]);
